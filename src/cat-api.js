3334077693b86a5d758b6e900bda0f2b948dc7ed
mellesacha <0251817@gmail.com>
2023-06-19 21:15:36 +0400

    hw10-3

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,27 @@
-const KEY = 'live_s5ZhN7B1SEFMCrAKbYl4oedjpzWq5pt8HyKxIm7SHCGgDJ2cV1IbtOJ03aGfWn6d';
+const API_KEY = 'live_s5ZhN7B1SEFMCrAKbYl4oedjpzWq5pt8HyKxIm7SHCGgDJ2cV1IbtOJ03aGfWn6d';
+const BASE_URL = 'https://api.thecatapi.com/v1'
 
 function fetchBreeds() {
-    return fetch('https://api.thecatapi.com/v1/breeds?api_key=KEY')
-        .then(response => response.json())
+    return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`)
+        .then(response => {
+
+            if (!response.ok) {
+                throw new Error()
+            }
+            return response.json()
+        }
+        )
 };
 
-function fetchCatByBreed(breedId) {return fetch(`https://api.thecatapi.com/v1/images/${breedId}?api_key=KEY&has_breeds=1`)
-        .then(response => response.json())}
+function fetchCatByBreed(breedId) {return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
+        .then(response => {
+
+            if (!response.ok) {
+                throw new Error()
+            }
+            return response.json()
+        }
+        )
+};
 
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -5,74 +5,55 @@ const loader = document.querySelector('.backdrop');
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
-
-// new SlimSelect({
-//     select: '.breed-select',
-//     settings: {
-//     placeholderText: 'Select a cat breed',
-//     hideSelected: true,
-//   },
-//     events: {
-//         beforeChange: btnSelectBread.insertAdjacentHTML("beforeend", `<option>3</option><option>2</option>`)
-//     }
-// })
-
-// btnSelectBread.style.display = "none";
-// articleCat.style.display = "none";
-
 btnSelectBread.classList.add('is-hidden');
 articleCat.classList.add('is-hidden');
 
 
 fetchBreeds().
     then(onMarkupList).
-    catch(error => Notify.failure('Oops! Something went wrong! Try reloading the page!', {timeout: 100000}));
+    catch(onError);
 
 function onMarkupList(array) {
     array.forEach(e => btnSelectBread.insertAdjacentHTML("beforeend",
-        `<option value=${e.reference_image_id}>${e.name}</option>`));
+        `<option value=${e.id}>${e.name}</option>`));
     
-    // btnSelectBread.style.display = "block";
-    // loader.style.display = "none";
 
     btnSelectBread.classList.remove('is-hidden');
     loader.classList.add('is-hidden');
 
 }
 
+
 btnSelectBread.addEventListener('change', onSelect);
 
 function onSelect() {
-
-    // loader.style.display = "block";
-    // articleCat.style.display = "none";
 
     loader.classList.remove('is-hidden');
     articleCat.classList.add('is-hidden');
 
 
     const breedId = btnSelectBread.value;
+    console.log(breedId)
 
     fetchCatByBreed(breedId).
         then(onCreateCatProfile).
-        catch(error => Notify.failure('Oops! Something went wrong! Try reloading the page!', {timeout: 100000}));
+        catch(onError)
+        
     
 }
 
-{/* <p>${breeds[0].name}</p><p>${breeds[0].description}</p><p>Temperament: ${breeds[0].temperament}</p> */ }
-{/* <ul class=""><li>${breeds[0].name}</li><li>${breeds[0].description}</li><li>Temperament: ${breeds[0].temperament}</li></ul>` */}
-
-function onCreateCatProfile(obj) {
-    const { url, breeds } = obj;
+function onCreateCatProfile(arr) {
+    const { url, breeds } = arr[0];
     articleCat.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400" /><div class="cat-list"><p class="cat-name">${breeds[0].name}</p class="cat-about"><p>${breeds[0].description}</p><p><span class="cat-temp">Temperament: </span>${breeds[0].temperament}</p></div>`;
-    // loader.style.display = "none";
-    // articleCat.style.display = "block";
 
     loader.classList.add('is-hidden');
     articleCat.classList.remove('is-hidden');
- 
+  
+}
+
+function onError() {
+    Notify.failure('Oops! Something went wrong! Try reloading the page!', { timeout: 100000 });
+    loader.classList.add('is-hidden')
 }
 
 
